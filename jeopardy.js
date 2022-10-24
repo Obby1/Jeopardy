44ac3846a53ca7af522018f7eeee3ef11c9b0efc
Obby <obbydelrosario@gmail.com>
2022-10-23 17:10:35 -0700

    added js to fix event handler and html table

--- jeopardy.js
+++ jeopardy.js
@@ -19,8 +19,13 @@
 //  ]
 
 let categories = [];
+let allCategoriesArr = [];
+let catObj = [];
+// let categoryNames = [];
 let count = 0;
 
+//todo: ?Give IDs to each object. Assign corresponding IDs to html table. Access objects by
+// clicking on the HTML ID with event handler? 
 
 /** Get NUM_CATEGORIES random category from API.
  *
@@ -30,20 +35,19 @@ let count = 0;
 async function getCategoryIds() {
 count += 6;
 categories = [];
+tempCats = [];
 let response = await axios.get(`https://jservice.io/api/categories`, {params: {count:100, offset:count}});
-// console.log(response.data);
-// let categories = response.data.map(el=>({
-//     id: el.id
-// }))
 response.data.map(el=>{
     // console.log(el.id)
-    categories.push(el.id);
+    tempCats.push(el.id);
 })
-let randomCats= _.sampleSize(categories, 6);
+let randomCats= _.sampleSize(tempCats, 6);
+categories.push(randomCats);
+console.log(`categories array is ${categories}`)
+// console.log(`randomCats is ${randomCats}`);
+return categories;
 
-// console.log(randomCats);
-return randomCats;
-
+//todo: further optimize this code. Could be a way to shorten logic
 }
 
 /** Return object with data about a category:
@@ -58,24 +62,40 @@ return randomCats;
  *   ]
  */
 
+
 async function getCategory(catId) {
 let response = await axios.get(`https://jservice.io/api/category?id=${catId}`);
 // console.log(response.data);
-
 let tempObj = {}
 tempObj.title= response.data.title;
+// tempObj.showing = `?`,
 // tempObj.clues= response.data.clues;
 tempObj.clues = response.data.clues.map(el=>({
 question: el.question,
 answer: el.answer,
-showing:null,
+showing: `?`,
 }));
 // tempObj.clues = clues;
 // console.log(clues);
-return tempObj;
 // console.log(tempObj);
-
+return tempObj;
 }
+
+//how do I change temp Obj name to [0-0] idx? 
+async function allCategories([categories]){
+    for (let category of categories){
+        // console.log(category);
+       let tempObj= await getCategory(category);
+    //    console.log(tempObj);
+    allCategoriesArr.push(tempObj);
+    }
+}
+
+// function getAllCats(categories){
+// let allCats = categories.map(el => {
+//     return getCategory(el)});
+// console.log(allCats);
+// }
 
 /** Fill the HTML table#jeopardy with the categories & cells for questions.
  *
@@ -85,9 +105,146 @@ return tempObj;
  *   (initally, just show a "?" where the question/answer would go.)
  */
 
-async function fillTable() {
+async function fillTable(categories) {
+    let $table = $(`#jeopardy`);
+    let $htmlTable = $(`
+    <table border = 1>
+        <thead>
+            <tr>
+                <th class="title">
+                ${allCategoriesArr[0].title}
+               
+                </th>
+                <th class="title">
+                ${allCategoriesArr[1].title}
+                </th>
+                <th class="title">
+                ${allCategoriesArr[2].title}
+                </th>
+                <th class="title">
+                ${allCategoriesArr[3].title}
+                </th>
+                <th class="title">
+                ${allCategoriesArr[4].title}
+                </th>
+                <th class="title">
+                ${allCategoriesArr[5].title}
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="question" data-question-id="0" data-clues-id="0">
+                ${allCategoriesArr[0].clues[0].showing}
+                </td>
+                <td class="question" data-question-id="1" data-clues-id="0">
+                ${allCategoriesArr[1].clues[0].showing}
+                </td>
+                <td class="question" data-question-id="2" data-clues-id="0">
+                ${allCategoriesArr[2].clues[0].showing}
+                </td>
+                <td class="question" data-question-id="3" data-clues-id="0">
+                ${allCategoriesArr[3].clues[0].showing}
+                </td>
+                <td class="question" data-question-id="4" data-clues-id="0">
+                ${allCategoriesArr[4].clues[0].showing}
+                </td>
+                <td class="question" data-question-id="5" data-clues-id="0">
+                ${allCategoriesArr[5].clues[0].showing}
+                </td>
+            </tr>
+            <tr>
+                <td class="question" data-question-id="0" data-clues-id="1"> 
+                ${allCategoriesArr[0].clues[1].showing}
+                </td>
+                <td class="question" data-question-id="1" data-clues-id="1">
+                ${allCategoriesArr[1].clues[1].showing}
+                </td>
+                <td class="question" data-question-id="2" data-clues-id="1">
+                ${allCategoriesArr[2].clues[1].showing}
+                </td>
+                <td class="question" data-question-id="3" data-clues-id="1">
+                ${allCategoriesArr[3].clues[1].showing}
+                </td>
+                <td class="question" data-question-id="4" data-clues-id="1">
+                ${allCategoriesArr[4].clues[1].showing}    
+                </td>
+                <td class="question" data-question-id="5" data-clues-id="1">
+                ${allCategoriesArr[5].clues[1].showing}
+                </td>
+            </tr>
+                <tr>
+                <td class="question" data-question-id="0" data-clues-id="2">
+                ${allCategoriesArr[0].clues[2].showing}
+                </td>
+                <td class="question" data-question-id="1" data-clues-id="2">
+                ${allCategoriesArr[1].clues[2].showing}
+                </td>
+                <td class="question" data-question-id="2" data-clues-id="2">
+                ${allCategoriesArr[2].clues[2].showing}
+                </td>
+                <td class="question" data-question-id="3" data-clues-id="2">
+                ${allCategoriesArr[3].clues[2].showing}
+                </td>
+                <td class="question" data-question-id="4" data-clues-id="2">
+                ${allCategoriesArr[4].clues[2].showing}     
+                </td>
+                <td class="question" data-question-id="5" data-clues-id="2">
+                ${allCategoriesArr[5].clues[2].showing}
+                </td>
+            </tr>
+                <tr>
+                <td class="question" data-question-id="0" data-clues-id="3">
+                ${allCategoriesArr[0].clues[3].showing}
+                </td>
+                <td class="question" data-question-id="1" data-clues-id="3">
+                ${allCategoriesArr[1].clues[3].showing}
+                </td>
+                <td class="question" data-question-id="2" data-clues-id="3">
+                ${allCategoriesArr[2].clues[3].showing}
+                </td>
+                <td class="question" data-question-id="3" data-clues-id="3">
+                ${allCategoriesArr[3].clues[3].showing}
+                </td>
+                <td class="question" data-question-id="4" data-clues-id="3">
+                ${allCategoriesArr[4].clues[3].showing}     
+                </td>
+                <td class="question" data-question-id="5" data-clues-id="3">
+                ${allCategoriesArr[5].clues[3].showing}
+                </td>
+            </tr>
+            <tr>
+                <td class="question" data-question-id="0" data-clues-id="4">
+                ${allCategoriesArr[0].clues[4].showing}
+                </td>
+                <td class="question" data-question-id="1" data-clues-id="4">
+                ${allCategoriesArr[1].clues[4].showing}
+                </td>
+                <td class="question" data-question-id="2" data-clues-id="4">
+                ${allCategoriesArr[2].clues[4].showing}
+                </td>
+                <td class="question" data-question-id="3" data-clues-id="4">
+                ${allCategoriesArr[3].clues[4].showing}
+                </td>
+                <td class="question" data-question-id="4" data-clues-id="4">
+                ${allCategoriesArr[4].clues[4].showing}    
+                </td>
+                <td class="question" data-question-id="5" data-clues-id="4">
+                ${allCategoriesArr[5].clues[4].showing}
+                </td>
+            </tr>
+        
+    
+    
+        </tbody>
+    </table>
+    
+    `)
+    $table.append($htmlTable);
+    
+    }
 
-}
+
 
 /** Handle clicking on a clue: show the question or answer.
  *
@@ -97,8 +254,54 @@ async function fillTable() {
  * - if currently "answer", ignore click
  * */
 
-function handleClick(evt) {
+// function handleClick(evt) {
+//     console.log(evt.target.id);
+// }
+
+//what if I used 2 data attributes in table? 1 for categories arr 1 for clues arr
+//allCategories[0].clues[0]
+//evt.target.closest.question.data"cluesid"
+//need to change .showing to updated property and also refresh that dom element. JQ
+//or a recent unit has funcitonality for this. 
+
+//access clues array with clueId - clues[clueId] = {cluesobject.question etc}
+// let clue = clues[event.target.dataset.clueId];
+// how to assign clueId dynamically after getting clues from API?
+$(`#jeopardy`).on("click", function handleClick(evt){
+// let clue = e.target.dataset.id;
+let questionId = $(evt.target).closest(".question").data("question-id");
+let clueId = $(evt.target).closest(".question").data("clues-id");
+console.log(`jQ question id ${questionId}`);
+console.log(`jQ clue id ${clueId}`);
+console.log(allCategoriesArr[questionId].clues[clueId]);
+
+//if showing is ? set to clue question
+if (allCategoriesArr[questionId].clues[clueId].showing === `?`){
+    allCategoriesArr[questionId].clues[clueId].showing = allCategoriesArr[questionId].clues[clueId].question;
+    console.log(`? -> question working`);
+    console.log(allCategoriesArr[questionId].clues[clueId].question);
+    let $selection = $(evt.target).closest(".question");
+    // $selection.html(`test`);
+    $selection.html(allCategoriesArr[questionId].clues[clueId].showing);
+} //if showing is set to question, set showing to answer
+else if (allCategoriesArr[questionId].clues[clueId].showing === allCategoriesArr[questionId].clues[clueId].question){
+    allCategoriesArr[questionId].clues[clueId].showing = allCategoriesArr[questionId].clues[clueId].answer;
+    console.log(`question -> answer working`);
+    console.log(allCategoriesArr[questionId].clues[clueId].answer);
+    let $selection = $(evt.target).closest(".question");
+    $selection.html(allCategoriesArr[questionId].clues[clueId].showing);
+} else if (allCategoriesArr[questionId].clues[clueId].showing === allCategoriesArr[questionId].clues[clueId].answer){
+   console.log(`last step`)
+    return;
 }
+
+// allCategoriesArr[questionId].clues[clueId].showing = allCategoriesArr[questionId].clues[clueId].question;
+
+
+//clean up code by assigned allcatarr etc to a shorter variable. Make it easier to read. 
+});
+
+
 
 /** Wipe the current Jeopardy board, show the loading spinner,
  * and update the button used to fetch data.
@@ -121,7 +324,11 @@ function hideLoadingView() {
  * */
 
 async function setupAndStart() {
+    await getCategoryIds();
+    await allCategories(categories);
+    await fillTable(categories);
 }
+setupAndStart()
 
 /** On click of start / restart button, set up game. */
 
@@ -130,3 +337,49 @@ async function setupAndStart() {
 /** On page load, add event handler for clicking clues */
 
 // TODO
+
+//1. Only load categories with questions that have length of 5. 
+//2. Catch/ try if error load page again 
+
+    // getCategoryIds();
+    // allCategories(categories);
+    // fillTable(categories);
+
+
+
+
+
+
+
+//below code loops through each category, not sure if this is what I want
+// async function fillTable([categories]) {
+//     for (let category of categories){
+//         console.log(`loop`);
+//         // console.log(categories);
+//         console.log(category);
+//     }
+
+// }
+
+
+
+
+
+    
+
+// async function fillTable(categories) {
+//     let $table = $(`#jeopardy`);
+    
+//     }
+
+// $(`#jeopardy`).on("click", async function handleClick(e){
+//     // let target = $(e.target).closest(".question");
+//     let target = $(e.target).closest(".question");
+//     console.log(target);
+//     //how do I access the object with an event click?
+//     //access innerHTML somehow? and run logic to check if ob
+// });
+
+//  <td class="question" data-id="5-0">
+// ${allCategoriesArr[5].showing}
+// </td>
